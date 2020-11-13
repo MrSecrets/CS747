3505d87890c01ea1299508eeea93d848b16576f4
MrSecrets <sreerajsj10@hotmail.com>
2020-11-13 19:40:51 +0530

    implemented Sarsa

--- Windy_Gridworld/agent.py
+++ Windy_Gridworld/agent.py
@@ -1,61 +1,131 @@
 import numpy as np
-import argsparse
+import argparse
 import matplotlib.pyplot as plt
 import windy_gridworld
 
-def sarsa_choose(Q,epsilon,action_space,state):
+max_steps=200
+episodes = 200
+episode_axis = np.arange(episodes)
+# time_total= 0
+
+def sarsa_choose(epsilon,action_space,state):
 	#state os current location
     action=0
+    global Q
     if np.random.uniform(0, 1) < epsilon: 
         action = np.random.choice(action_space) 
+        # print(action)
     else: 
-        action = np.argmax(Q[state, :]) 
+        action = np.argmax(Q[state[0],state[1] ,:])
+        # print(Q[state[0],state[1],:]) 
     return action 
 
 def update_sarsa(state, state2, reward, action, action2): 
-    predict = Q[state, action] 
-    target = reward + gamma * Q[state2, action2] 
-    Q[state, action] = Q[state, action] + alpha * (target - predict) 
+    gamma = 0.95
+    alpha = 0.85
+    global Q
+    predict = Q[state[0],state[1], action] 
+    # print("state2:   ", state2, action2)
+    target = reward + gamma * Q[state2[0],state2[1], action2] 
+    Q[state[0],state[1], action] = Q[state[0],state[1], action] + alpha * (target - predict) 
 
-def sarsa(eps,alpha,n_actions,episodes,stoch_bool):
-	Q = np.zeros((dims,n_actions))
-	max_steps=100
+def sarsa(eps,n_actions,stoch_bool):
+	# global time_total
+	global time_axis
+	global Q
+	global episodes
+	global n_action
+	for rs in range(0,10):
+		Q = np.zeros((7,10,n_actions))
+		np.random.seed(rs)
+		time_total = 0
+		for episode in range(episodes):
+			# print(episode)
+			grid = windy_gridworld.windy_gridworld()
+			state_1 = grid.current_loc
+			action_1 = sarsa_choose(eps,n_actions,state_1)
+			total_reward=0
+			t =0
+			# print(time_total)
+			# while t<max_steps:
+			while True:
+				stoch = 0
+				
+				if n_actions == 4:
+					if stoch_bool == 1:
+						wind_stoch = np.random.rand(1)[0]
+						if wind_stoch <=(1.0/3):
+							stoch =1
+						if wind_stoch >=(2.0/3):
+							stoch = -1
+					reward,state_2, done = grid.move_grid_4(action_1,stoch)
+				
+				if n_actions == 8:
+					if stoch_bool == 1:
+						wind_stoch = np.random.rand(1)[0]
+						if wind_stoch <=(1.0/3):
+							stoch =1
+						if wind_stoch >=(2.0/3):
+							stoch = -1				
+					reward,state_2, done = grid.move_grid_8(action_1,stoch)
 
-	for episode in range(episodes):
-		grid = windy_gridworld.windy_gridworld()
-		action = sarsa_choose(Q,eps,n_actions,loc)
-		total_reward=0
-		
-		while t<max_steps:
-			stoch = 0
-			
-			if n_actions == 4:
-				if stoch_bool = 1:
-					wind_stoch = np.random.rand(1)[0]
-					if wind_stoch <=(1.0/3):
-						stoch =1:
-					if wind_stoch >=(2.0/3):
-						stoch = -1:
-				reward,state_1, done = windy_gridworld.move_grid_4(action,stoch)
-			
-			if n_actions == 8:
-				if stoch_bool = 1:
-					wind_stoch = np.random.rand(1)[0]
-					if wind_stoch <=(1.0/3):
-						stoch =1:
-					if wind_stoch >=(2.0/3):
-						stoch = -1:				
-				reward,state_1, done = windy_gridworld.move_grid_8(action,stoch)
+				total_reward+=reward
+				action_2 = sarsa_choose(eps,n_actions,state_1)
+				update_sarsa(state_1, state_2, reward, action_1, action_2)
+				# print(state_1)
+				# print(action_1)
+				# print(Q[state_1[0],state_1[1],action_1])
+				# if state_1[1]>=6: print(state_1, action_1)
+				# if state_1 == [5,6]:
+					# print("sdfghjmjhgfdsfgh")
+				state_1 = state_2
+				action_1 = action_2
 
-			total_reward+=reward
-			action_2 = sarsa_choose(Q,eps,n_actions,new_loc)
-            update_sarsa(state_1, state_2, reward, action_1, action_2)
-            state_1 = state_2
-            action_1 = action_2
-            
-            t+=1
-            if done:
-            	break
+				t+=1
+				time_total += 1
+				if done:
+					print(episode)
+					print(time_total)
+					# print("dfghjklkjhgfdsdfghj,kjhgfdsdfghjhgfdsdfghjhgf")
+					break
+			time_axis[episode][rs]=time_total
 
+plt.figure()
 
+# Plotting 4 move with without stochastic wind
+n_action = 4
+Q = np.zeros((7,10,n_action)) #dimension of grid
+time_axis = np.zeros((episodes,10))
+sarsa(0.5,n_action,0)
+time_axis=np.mean(time_axis,axis=1)
+plt.plot(time_axis,episode_axis, label='4 move no stochastic wind')
 
+# Plotting 4 move with stochastic wind
+n_action = 4
+Q = np.zeros((7,10,n_action)) #dimension of grid
+time_axis = np.zeros((episodes,10))
+sarsa(0.5,n_action,1)
+time_axis=np.mean(time_axis,axis=1)
+plt.plot(time_axis,episode_axis, label='4 move with stochastic wind')
+
+# Plottinh kings move without stochastic wind
+n_action = 8
+Q = np.zeros((7,10,n_action)) #dimension of grid
+time_axis = np.zeros((episodes,10))
+sarsa(0.5,n_action,0)
+time_axis=np.mean(time_axis,axis=1)
+plt.plot(time_axis,episode_axis, label="king's move no stochastic wind")
+
+# PLotting kings move with stochstic wind
+n_action = 8
+Q = np.zeros((7,10,n_action)) #dimension of grid
+time_axis = np.zeros((episodes,10))
+sarsa(0.5,n_action,1)
+time_axis=np.mean(time_axis,axis=1)
+plt.plot(time_axis,episode_axis, label="king's move with stochastic wind")
+
+plt.xlabel("time step")
+plt.ylabel("episodes")
+plt.title("Task 2 ,3,4")
+plt.legend()
+plt.show()
--- Windy_Gridworld/windy_gridworld.py
+++ Windy_Gridworld/windy_gridworld.py
@@ -4,23 +4,24 @@ class windy_gridworld():
 
 	# def windy_gridworld(dims=[7,10] ,wind=0, start=[4,0],goal=[4,10]):
 	def __init__(self):
-	super(windy_gridworld,self).__init__()
+		super(windy_gridworld,self).__init__()
 		self.dims = (7,10)
-		self.env = np.zeros(dims)		
+		self.env = np.zeros(self.dims)		
 		self.wind = np.array([0,0,0,1,1,1,2,2,1,0])
-		self.start = [4,0]
-		self.goal = [4,9]
+		# self.wind = np.array([0,0,0,0,0,0,0,0,0,0])
+		self.start = [3,0]
+		self.goal = [3,9]
 
 		# check grid
-		if len(wind)!=dims[1]:
+		if len(self.wind)!=self.dims[1]:
 			return invalid
-		elif start[0]>dims[0] and start[0]>=0:
+		elif self.start[0]>self.dims[0] and self.start[0]>=0:
 			return invalid
-		elif start[1]>dims[1] and start[1]>=0:
+		elif self.start[1]>self.dims[1] and self.start[1]>=0:
 			return invalid
-		elif goal[0]>dims[0] and goal[0]>=0:
+		elif self.goal[0]>self.dims[0] and self.goal[0]>=0:
 			return invalid
-		elif goal[1]>dims[1] and goal[1]>=0:
+		elif self.goal[1]>self.dims[1] and self.goal[1]>=0:
 			return invalid
 
 		self.current_loc = self.start
@@ -31,9 +32,9 @@ class windy_gridworld():
 	# north = 0, east = 1, south = 2, west = 3
 		loc = self.current_loc
 		if action==0:
-			loc[0] -= 1:
+			loc[0] -= 1
 		elif action==1:
-			loc[1]+=1:
+			loc[1]+=1
 		elif action ==2:
 			loc[0]+=1
 		elif action==3:
@@ -43,23 +44,24 @@ class windy_gridworld():
 
 		if self.current_loc==self.goal:
 			self.status = 1
-			return -1, self.current_loc , self.status
+			return 100, self.current_loc , self.status
 
 
-		loc[0]-=self.wind(loc[0]) + stoch
+		loc[0]-=self.wind[loc[0]] + stoch
 
-		loc[0] = 0 if loc[0]<0
-		loc[1] = 0 if loc[1]<0
-		loc[0] = self.dims[0] if loc[0]>self.dims[0]
-		loc[1] = self.dims[1] if loc[1]>self.dims[1]
+		if loc[0]<0: loc[0] = 0 
+		if loc[1]<0: loc[1] = 0 
+		if loc[0]>=self.dims[0]: loc[0] = self.dims[0]-1
+		if loc[1]>=self.dims[1]: loc[1] = self.dims[1]-1 
 
 		self.current_loc=loc
-		if self.current_loc==self.goal:
+		if self.current_loc==self.goal: 
 			self.status = 1
-		
+			return 100, self.current_loc , self.status
+
 		return -1, self.current_loc , self.status
 
-	def move_grid_8(self,action, loc,stoch):
+	def move_grid_8(self,action,stoch):
 	# north = 0, ne = 1, east = 2, se = 3 south=4, sw = 5, west=6, nw=7
 		loc = self.current_loc
 		if action==0:
@@ -87,19 +89,19 @@ class windy_gridworld():
 
 		if self.current_loc==self.goal:
 			self.status = 1
-			return -1, self.current_loc , self.status
+			return 100, self.current_loc , self.status
 
-		loc[0]-=self.wind(loc[0]) + stoch
+		loc[0]-=self.wind[loc[0]] + stoch
 
-		loc[0] = 0 if loc[0]<0
-		loc[1] = 0 if loc[1]<0
-		loc[0] = self.dims[0] if loc[0]>self.dims[0]
-		loc[1] = self.dims[1] if loc[1]>self.dims[1]
+		if loc[0]<0: loc[0] = 0 
+		if loc[1]<0: loc[1] = 0 
+		if loc[0]>=self.dims[0]: loc[0] = self.dims[0]-1
+		if loc[1]>=self.dims[1]: loc[1] = self.dims[1]-1 
 
-		self.current_loc = loc
-
-		if self.current_loc==self.goal:
+		self.current_loc=loc
+		if self.current_loc==self.goal: 
 			self.status = 1
+			return 100, self.current_loc , self.status
 
 		
 		return -1 , self.current_loc, self.status
